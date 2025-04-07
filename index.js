bc745f1bf1c3cbb7bb59f0bc65bb91b42d9f8cd9
chientrm <chientrm@gmail.com>
2025-04-07 10:58:25 +0700

    feat: add express static middleware for profile route

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import "dotenv/config";
+import express from "express";
 import api from "./api.js";
 
 const PORT = process.env.PORT || 3000;
 
+api.use("/profile", express.static("dist"));
 api.listen(PORT, () => {
   console.log(`API server is running on port ${PORT}`);
 });
